d81b9091bc73289989417a3bcba1c214d28b76ba
Matheus <matheusfred95@gmail.com>
2026-03-25 15:47:02 -0300

    mudança nas imagens

--- src/data/Order.ts
+++ src/data/Order.ts
@@ -1,58 +1,60 @@
-type Order={
-    id:number,
-    name:string,
-    restaurant:string,
-    price:number,
-    image:string
+// 1. Importe todas as imagens primeiro
+import burgerImg from "../assets/burguer.jpg";
+import dogImg from "../assets/dog.jpg";
+import pizzaImg from "../assets/pizza.jpg";
+import sushiImg from "../assets/sushi.jpg";
+import tacoImg from "../assets/taco.jpg";
+import noodleImg from "../assets/noodle.jpg";
+
+export type Order = {
+    id: number;
+    name: string;
+    restaurant: string;
+    price: number;
+    image: string;
 }
 
-
-export const orders: Order[]=[
-
+export const orders: Order[] = [
     {
-        id:1,
-        name:"Mega Smash",
-        restaurant:"Burguer House",
-        price:49.90,
-        image:"src/assets/burguer.jpg"
-    },
-
-     {
-        id:2,
-        name:"Prime Dog",
-        restaurant:"Master Dog",
-        price:35.00,
-        image:"src/assets/dog.jpg"
-    },
-
-    {
-        id:3,
-        name:"Pizza de Calabresa",
-        restaurant:"Pizão",
-        price:50.00,
-        image:"src/assets/pizza.jpg"
+        id: 1,
+        name: "Mega Smash",
+        restaurant: "Burguer House",
+        price: 49.90,
+        image: burgerImg 
     },
     {
-        id:4,
-        name:"Sushi",
-        restaurant:"Samurai Sushi",
-        price:60.00,
-        image:"src/assets/sushi.jpg"
+        id: 2,
+        name: "Prime Dog",
+        restaurant: "Master Dog",
+        price: 35.00,
+        image: dogImg
     },
     {
-        id:5,
-        name:"Taco",
-        restaurant:"Taco Tuesday",
-        price:30.00,
-        image:"src/assets/taco.jpg"
+        id: 3,
+        name: "Pizza de Calabresa",
+        restaurant: "Pizão",
+        price: 50.00,
+        image: pizzaImg
     },
     {
-        id:6,
-        name:"Macarrão",
-        restaurant:"Noodle House",
-        price:30.00,
-        image:"src/assets/noodle.jpg"
+        id: 4,
+        name: "Sushi",
+        restaurant: "Samurai Sushi",
+        price: 60.00,
+        image: sushiImg
+    },
+    {
+        id: 5,
+        name: "Taco",
+        restaurant: "Taco Tuesday",
+        price: 30.00,
+        image: tacoImg
+    },
+    {
+        id: 6,
+        name: "Macarrão",
+        restaurant: "Noodle House",
+        price: 30.00,
+        image: noodleImg
     },
 ]
-
-    
--- src/data/Restaurants.ts
+++ src/data/Restaurants.ts
@@ -1,24 +1,35 @@
+import restauranteImg from "../assets/restaurante.jpg";
+import bistroImg from "../assets/bistro.jpg";
+import rangoImg from "../assets/rango.jpg";
 
-export const restaurants =[
+export type Restaurant = {
+  id: number;
+  name: string;
+  image: string;
+  location: string;
+  price: number;
+}
+
+export const restaurants: Restaurant[] = [
     {
-        id:1,
-        name:"Restaurante Sabor & Arte",
-        image:"src/assets/restaurante.jpg",
-        location:"Avenida Central 3000 Campo Grande - MS",
-        price:49.90
+        id: 1,
+        name: "Restaurante Sabor & Arte",
+        image: restauranteImg, 
+        location: "Avenida Central 3000 Campo Grande - MS",
+        price: 49.90
     },
     {
-        id:2,
-        name:"Bistrô Aurora",
-        image:"src/assets/bistro.jpg",
-        location:"Rua das Hortênsias Campo Grande - MS",
-        price:70.00
+        id: 2,
+        name: "Bistrô Aurora",
+        image: bistroImg, 
+        location: "Rua das Hortênsias Campo Grande - MS",
+        price: 70.00
     },
     {
-        id:3,
-        name:"Rango do Zeca",
-        image:"src/assets/rango.jpg",
-        location:"Travessa São Miguel Campo Grande - MS",
-        price:19.90 
+        id: 3,
+        name: "Rango do Zeca",
+        image: rangoImg,
+        location: "Travessa São Miguel Campo Grande - MS",
+        price: 19.90 
     }
 ]
